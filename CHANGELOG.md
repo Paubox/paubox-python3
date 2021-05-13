68d0a91c88afc31cd794adb6ba7ae58e45d906e2
niwong <nick@nickwong.io>
2021-05-12 17:03:58 -0700

    Update README and setup.py. Create a changelog.

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+# v1.0.0 / 2021-05-12
+### 🚀 Major Release
+This is the first release of the official Paubox-python3 package. This package is not intended to be backwards compatible with Python2, and users who are using Python2 should navigate to our Python2 [paubox-python](https://github.com/Paubox/paubox-python) package.
+
+- [#1](https://github.com/Paubox/paubox-python3/pull/1) Build a working version of the Paubox-python3 SDK. ([@niwong](https://github.com/niwong))
--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pauboxpython3",
+    name="paubox-python3",
     version="1.0.0",
     author="Paubox",
     author_email="info@paubox.com",
-    description="Python SDK for Paubox Email REST API",
+    description="Python3 SDK for Paubox Email REST API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Paubox/paubox-python3",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.8.2",
         "License :: OSI Approved :: Apache Software License"
     ],
 )
