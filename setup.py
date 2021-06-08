291e7ca6315538cba74a4aa69f847e2a071a9150
niwong <nick@nickwong.io>
2021-06-08 16:10:46 -0700

    Bump version number and indicate development release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="paubox-python3",
-    version="1.0.0",
+    version="1.0.1.dev",
     author="Paubox",
     author_email="info@paubox.com",
     description="Python3 SDK for Paubox Email REST API",
